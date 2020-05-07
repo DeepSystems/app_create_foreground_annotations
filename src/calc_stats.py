0b04291e5e0d68b6cde8adae8d0c2b41910eeab2
Maxim <max@supervise.ly>
2020-05-07 14:11:23 +0300

    wip

--- src/calc_stats.py
+++ src/calc_stats.py
@@ -4,11 +4,11 @@ from supervisely_lib.io.json import load_json_file, dump_json_file
 
 
 
-state = None
-if sly.fs.file_exists(sly.TaskPaths.TASK_CONFIG_PATH):
-    app_config = load_json_file(sly.TaskPaths.TASK_CONFIG_PATH)
-    state = app_config[STATE]
-else:
+# state = None
+# if sly.fs.file_exists(sly.TaskPaths.TASK_CONFIG_PATH):
+#     app_config = load_json_file(sly.TaskPaths.TASK_CONFIG_PATH)
+#     state = app_config[STATE]
+# else:
 
 
 
