722e8559215bc9856328a38ef7cf0f0b7bf92913
Maxim <max@supervise.ly>
2020-05-07 16:25:52 +0300

    remove comments from html

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 
 def read_projects(api, workspace_id):
     res = []
+    #_ids = [ ]
     projects = api.project.get_list(workspace_id)
     for idx, project in enumerate(projects):
         item = {}
         item["id"] = project.id
+        #_ids.append(project.id)
         item["name"] = project.name
         item["imagesCount"] = api.project.get_images_count(project.id)
         item["index"] = idx
